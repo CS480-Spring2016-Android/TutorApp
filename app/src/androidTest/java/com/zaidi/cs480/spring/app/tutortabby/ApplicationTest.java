36030835246a778a6c3e4a1a4efd96f5c40b28f9
CheezBoiger <alexander.garcia18@yahoo.com>
2016-04-18 23:20:15 -0700

    init commit

---
+++ app/src/androidTest/java/com/zaidi/cs480/spring/app/tutortabby/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.zaidi.cs480.spring.app.tutortabby;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+  public ApplicationTest() {
+    super(Application.class);
+  }
+}

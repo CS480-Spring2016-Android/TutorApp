be8f583fca9e86573690a633387ea9fdc2cf76ea
jacobromero <jacob22romero@gmail.com>
2016-04-19 15:26:09 -0700

    Added profile activity
    Added a profile activity
    Added 'login' button, just starts profile activity right now

--- app/src/main/java/com/zaidi/cs480/spring/app/tutortabby/MainActivity.java
+++ app/src/main/java/com/zaidi/cs480/spring/app/tutortabby/MainActivity.java
@@ -1,7 +1,9 @@
 package com.zaidi.cs480.spring.app.tutortabby;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
+  }
+
+  protected void login(View view){
+    Intent intent = new Intent(this, Profile.class);
+    finish();
+    startActivity(intent);
   }
 }
---
+++ app/src/main/java/com/zaidi/cs480/spring/app/tutortabby/Profile.java
@@ -0,0 +1,13 @@
+package com.zaidi.cs480.spring.app.tutortabby;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
